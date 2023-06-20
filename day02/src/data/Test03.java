20ae955ce0843d3e43981bfa3a277f71e890be3c
lodorive <s_s0309@naver.com>
2023-06-20 12:52:27 +0900

    변수를 사용

---
+++ day02/src/data/Test03.java
@@ -0,0 +1,14 @@
+package data;
+
+public class Test03 {
+	public static void main(String[] args) {
+		// 변수(Variable) = 데이터 기억장치
+	
+		int a = 7000 * 2; // 7000 * 2을 계산해서 a라는 이름으로 저장
+		int b = 8000 * 3; // 8000 * 2을 계산해서 a라는 이름으로 저장
+		
+		System.out.println(a); // a에 저장된 데이터를 출력
+		System.out.println(b); // b에 저장된 데이터를 출력
+		System.out.println(a + b);
+	}
+}
