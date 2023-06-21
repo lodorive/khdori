4013f9199389008be3f388c4f094f9c3b7b133d1
lodorive <s_s0309@naver.com>
2023-06-21 10:16:29 +0900

    커피숍 문제 풀이(5+1)

--- day02/src/data/Test13.java
+++ day02/src/data/Test13.java
@@ -2,14 +2,15 @@ package data;
 
 public class Test13 {
 	public static void main(String[] args) {
-		//커피숍 문제 5+1 풀이
+		//커피숍 문제 풀이(5+1)
 		
 		//입력
 		int price = 2500;
 		int count = 28;
+		int event = 5;
 		
 		//계산
-		int free = count / 6;
+		int free = count / (event + 1);
 	    int pay = count - free;
 	    
 	    int total = price * pay;
