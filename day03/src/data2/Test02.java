51f2ca7f481a33a62cb7f394087b41cdc1c96e9c
lodorive <s_s0309@naver.com>
2023-06-21 13:07:42 +0900

    BMI 지수 구하기

--- day03/src/data2/Test02.java
+++ day03/src/data2/Test02.java
@@ -3,12 +3,13 @@ package data2;
 public class Test02 {
 	public static void main(String[] args) {
 		// 키 180cm, 몸무게 80kg의 BMI 지수 구하기
+		
 		// 입력
 		int cm = 180;
 		int kg = 80;
 
 		//int m = cm / 100;
-		//double m = cm/100d;	
+		//double m = cm/100d;
 		double m = (double)cm / 100;
 		
 		//계산
