a43cb5452006c25c7de5dd6f1d792209a347397c
lodorive <s_s0309@naver.com>
2023-06-22 13:34:12 +0900

    표준입력

---
+++ day04/src/input/Test01.java
@@ -0,0 +1,14 @@
+package input;
+
+import java.io.IOException;
+
+public class Test01 {
+	public static void main(String[] args) throws IOException {
+		//표준입력 
+		//- System.in 을 사용하여 처리
+		//- 사용자가 입력해주기를 기다려야 한다
+		//- ASCII 코드를 입력 받는다
+		
+		System.in.read();
+	}
+}
