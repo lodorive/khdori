c9c0c925413307ebba48b8f044d0c76238ac2978
lodorive <s_s0309@naver.com>
2023-06-22 09:52:01 +0900

    윤년 계산기 프로그램

--- day03/src/data3/Test07.java
+++ day03/src/data3/Test07.java
@@ -4,26 +4,27 @@ public class Test07 {
 	public static void main(String[] args) {
 		//윤년 계산기 프로그램
 		
+		//입력
 		int year = 2400;
 		
+		//계산
+		
+		//[1] 4의 배수면 윤년 include
+		//[2] 100의 배수가 아니어야 한다 exclude
+		//[3] 400의 배수면 윤년이다 include
+		
 		//4의 배수인 해
-		int year1 = year % 4; 
-		//System.out.println(year1);		
-		
+		boolean is4 = year % 4 == 0; 
 		//100의 배수인 해
-		int year2 = year % 100;
-		//System.out.println(year2);		
-		
+		//boolean isNot100 = year % 100 != 0;	
+		boolean isNot100 = !(year % 100 == 0);
+		//!= 대신 괄호 앞에 !를 붙여도 아니라는 의미
 		//400의 배수인 해
-		int year3 = year % 400;
-		//System.out.println(year3);		
+		boolean is400 = year % 400 == 0;
 		
-		boolean condition1 = year1 == 0;
-		boolean condition2 = year2 != 0;
-		boolean condition3 = year3 == 0;
-				
-		boolean leapyear = condition1 && condition2 || condition3;
+		boolean leapYear = is400 || is4 && isNot100;
 		
-		System.out.println(leapyear);
+		//출력
+		System.out.println(leapYear);
 	}
 }
