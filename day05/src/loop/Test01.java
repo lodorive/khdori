770ab2ac34015f802714becbf55c4a659d78ef92
lodorive <s_s0309@naver.com>
2023-06-23 17:16:33 +0900

    반복(loop) 예제

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//반복(loop)
+		//[Q] Hello를 화면에 10번 출력하세요
+		
+		for(int i=0; i < 10; i ++){
+			System.out.println("Hello!");			
+		}		
+	}
+}
