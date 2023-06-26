e7bce93651a8f3d500ac63d4f0b1be57f73c3da0
lodorive <s_s0309@naver.com>
2023-06-26 09:29:00 +0900

    주말 과제

--- day05/src/loop/Test04.java
+++ day05/src/loop/Test04.java
@@ -2,15 +2,15 @@ package loop;
 
 public class Test04 {
 	public static void main(String[] args) {
-		//1부터 99사이에 5가 포함되어 있는 숫자만 필터링하여 출력
+		//1부터 99 사이에 5가 포함되어 있는 숫자만 필터링하여 출력
 		
-		for(int i = 1; i <=99; i++) {
-			if(i / 10 == 5){ 
-			System.out.println(i);
+		for(int i = 1; i <99; i++){
+			if(i / 10 == 5) {
+				System.out.println(i);
 			}
-			else if(i % 10 == 5){
-			System.out.println(i);	
+			else if(i % 10 == 5) {
+				System.out.println(i);
 			}
-		}		
+		}
 	}
 }
--- day05/src/loop/Test05.java
+++ day05/src/loop/Test05.java
@@ -4,12 +4,16 @@ public class Test05 {
 	public static void main(String[] args) {
 		//1부터 99 사이에 369게임에서 박수치는 숫자만 필터링하여 출력
 		
-		for(int i = 1; i <=99; i++) {
-			if(i / 10 == 3 || i == 6 || i == 9){ 
+		for(int i = 1; i <= 99; i++){
+			
+			int ten = i / 10;
+			int one = i % 10;
+			
+			if(ten == 3 || ten == 6 || ten == 9){		
 			System.out.println(i);
 			}
-			else if(i % 10 == 3 || i == 6 || i == 9){
-				System.out.println(i);	
+			else if(one == 3 || one == 6 || one == 9) {
+			System.out.println(i);	
 			}
 		}		
 	}
