27351bd6b4bc59dfe773bb12380263bd0ec3d9c6
lodorive <s_s0309@naver.com>
2023-06-29 14:09:44 +0900

    버블 정렬 문제

--- day09/src/array2/Test02_4.java
+++ day09/src/array2/Test02_4.java
@@ -9,17 +9,15 @@ public class Test02_4 {
 		int[] data = new int[] {20,10,30,40,50};
 		
 		//버블 정렬 - k회차(인접한 두 개의 항목을 비교하여 큰 값을 우측으로 이동)
-		for(int k =data.length-2; k>=0; k--) {
-			for(int i = 0; i<=k; i++) {	
-				if(data[i] > data[i+1]);{
-					
+		for(int k=data.length-2; k >= 0; k--) {
+			for(int i=0; i <= k; i++) {
+				if(data[i] > data[i+1]){
 					int backup = data[i];
 					data[i] = data[i+1];
 					data[i+1] = backup;
 				}
 			}
-		}
-			
+		}	
 		
 		for(int i = 0; i<data.length; i++) {
 			System.out.print(data[i]);
