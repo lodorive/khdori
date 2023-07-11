b12f301d8bf06a45df2ebed49e564f06e60d0b4f
lodorive <s_s0309@naver.com>
2023-07-11 10:36:23 +0900

    실제 만나이 계산기 문제 풀이

---
+++ day16/src/api/exception/Test06_2.java
@@ -0,0 +1,17 @@
+package api.exception;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Test06_2 {
+	public static void main(String[] args) {
+		System.out.println(DateCalculator.getMonthDay(2023, 7));
+		
+		//만약에 2021-03-15일생이면
+		int total = 0;
+		total += 365 - DateCalculator.getYearDay(2023, 3, 15) +1;
+		total += 365;
+		total += DateCalculator.getYearDay(2023, 7, 11);
+		System.out.println("total = "+total);
+		}
+	}
