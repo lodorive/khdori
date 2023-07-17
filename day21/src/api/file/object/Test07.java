f95e5fe3d5344c6925613eb39ffb84b10c93813e
lodorive <s_s0309@naver.com>
2023-07-17 18:13:52 +0900

    게임 문제 풀이

---
+++ day21/src/api/file/object/Test07.java
@@ -0,0 +1,9 @@
+package api.file.object;
+
+public class Test07 {
+	public static void main(String[] args) {
+
+		Player player = new Player("테스트");
+		player.show();
+	}
+}
---
+++ day21/src/api/file/object/Test07_8.java
@@ -0,0 +1,13 @@
+package api.file.object;
+
+import java.io.IOException;
+
+public class Test07_8 {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		GameUtil.play();
+	}
+}
+
+
+
+
