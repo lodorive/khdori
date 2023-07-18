601769e3604a848339f73b8124d528bfe7f50e44
lodorive <s_s0309@naver.com>
2023-07-18 10:49:21 +0900

    콘솔 메모장 프로그램

---
+++ day22/src/api/file/string/Test03.java
@@ -0,0 +1,33 @@
+package api.file.string;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Test03 {
+	public static void main(String[] args) throws IOException{
+		//콘솔 메모장 프로그램
+		
+		File target = new File("sample/note.kh");
+		FileWriter fw = new FileWriter(target);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		while(true) {
+
+		Scanner sc = new Scanner(System.in);		
+		System.out.print("입력 : ");
+		String input = sc.nextLine();
+		
+		if(input.equals("종료")) break;
+		
+		bw.write(input);
+		bw.write("\n");
+		//flush를 하면 즉시 저장이 됨
+		bw.flush();
+		
+		}
+		bw.close();
+	}
+}
