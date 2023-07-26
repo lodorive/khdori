5bb595d7e84fd266dd6e861ce56ad34063d3445c
lodorive <s_s0309@naver.com>
2023-07-26 09:44:08 +0900

    도서 조회(상세)

--- jdbc03/src/jdbc/app/Test24.java
+++ jdbc03/src/jdbc/app/Test24.java
@@ -5,6 +5,7 @@ import jdbc.dto.BookDto;
 
 public class Test24 {
 	public static void main(String[] args) {
+		//도서 정보 상세 조회
 		
 		int bookId = 2;
 		
@@ -15,10 +16,10 @@ public class Test24 {
 			System.out.println("찾으시는 도서 번호가 없습니다");
 		}
 		else {
-			System.out.println("책 넘버 : "+dto.getBookId());
+			System.out.println("<도서 정보>");
 			System.out.println("책 제목 : "+dto.getBookTitle());
 			System.out.println("저자 : "+dto.getBookPublisher());
-			System.out.println("가격 : "+dto.getBookPrice()+"$");
+			System.out.println("가격 : $"+dto.getBookPrice());
 		}
 	}
 }
