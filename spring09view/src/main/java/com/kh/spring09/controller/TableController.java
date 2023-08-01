9dbb23f00e08589775924c54cc6d1451b55150e2
lodorive <s_s0309@naver.com>
2023-08-01 13:45:59 +0900

    구내식당 식단표 문제

--- spring09view/src/main/java/com/kh/spring09/controller/TableController.java
+++ spring09view/src/main/java/com/kh/spring09/controller/TableController.java
@@ -12,4 +12,9 @@ public class TableController {
 		return "/WEB-INF/views/table/test01.jsp";
 	}
 	
+	@RequestMapping("/test02")
+	public String test02() {
+		return "/WEB-INF/views/table/test02.jsp";
+	}
+	
 }
