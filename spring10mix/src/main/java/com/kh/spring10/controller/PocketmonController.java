5caec26a6f13dc321c6ad42ea5c62d2fdd390e58
lodorive <s_s0309@naver.com>
2023-08-07 10:22:38 +0900

    포켓몬 정보 페이지 구현

--- spring10mix/src/main/java/com/kh/spring10/controller/PocketmonController.java
+++ spring10mix/src/main/java/com/kh/spring10/controller/PocketmonController.java
@@ -83,4 +83,16 @@ public class PocketmonController {
 			return "redirect:에러페이지주소";
 		}
 	}
+	
+	//삭제
+	@RequestMapping("/delete")
+	public String delete(@RequestParam int no) {
+		boolean result = dao.delete(no);
+		if(result) {
+			return "redirect:list";
+				}
+		else {
+			return "redirect:에러페이지";
+		}
+	}
 }
