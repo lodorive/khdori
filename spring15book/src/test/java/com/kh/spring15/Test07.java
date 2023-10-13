f3bc00385885e5587bfcb21550c8798495c3ef53
lodorive <s_s0309@naver.com>
2023-10-13 13:46:56 +0900

    도서 DAO 구현

--- spring15book/src/test/java/com/kh/spring15/Test07.java
+++ spring15book/src/test/java/com/kh/spring15/Test07.java
@@ -34,7 +34,16 @@ public class Test07 {
 //		param.put("bookPageMin", 90);
 //		param.put("bookPageMax", 500);
 //		param.put("bookGenre", "판타지"); //장르가 한개였다면(X)
-		param.put("bookGenreList", List.of("판타지", "공포", "교양")); //장르가 여러개라면(O)
+//		param.put("bookGenreList", List.of()); //장르가 여러개라면(O)
+//		param.put("bookGenreList", List.of("판타지")); //장르가 여러개라면(O)
+//		param.put("bookGenreList", List.of("판타지", "공포")); //장르가 여러개라면(O)
+//		param.put("bookGenreList", List.of("판타지", "공포", "교양")); //장르가 여러개라면(O)
+		
+//		param.put("orderList", List.of("book_id asc")); //번호순
+//		param.put("orderList", List.of("book_publication_date desc", "book_id asc")); //최근 출간순(같으면 번호순)
+		
+		param.put("begin", 1);
+		param.put("end", 10);
 		
 		List<BookDto> list = sqlSession.selectList("book.complexSearch", param);
 		log.debug("검색 결과 = {}", list.size());
