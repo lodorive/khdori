f20da84404e944f03111a4003b7242dde58a38a4
lodorive <s_s0309@naver.com>
2023-10-20 10:12:14 +0900

    JSP 설정 추가

---
+++ spring20websocket/src/main/java/com/kh/spring20/controller/WebSocketViewController.java
@@ -0,0 +1,14 @@
+package com.kh.spring20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebSocketViewController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+}
