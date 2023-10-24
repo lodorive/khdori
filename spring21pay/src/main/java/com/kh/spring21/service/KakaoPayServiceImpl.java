f2c8680372c3d61b9f789136efe0d85838521ae3
lodorive <s_s0309@naver.com>
2023-10-24 14:31:10 +0900

    카카오 페이 결제 준비에 대한 모듈화 테스트 완료

---
+++ spring21pay/src/main/java/com/kh/spring21/service/KakaoPayServiceImpl.java
@@ -0,0 +1,58 @@
+package com.kh.spring21.service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import com.kh.spring21.configuration.KakaoPayProperties;
+import com.kh.spring21.vo.KakaoPayReadyRequestVO;
+import com.kh.spring21.vo.KakaoPayReadyResponseVO;
+
+@Service
+public class KakaoPayServiceImpl implements KakaoPayService{
+
+	@Autowired
+	private KakaoPayProperties kakaoPayProperties;
+	
+	@Autowired
+	private RestTemplate template;
+	
+	@Autowired
+	private HttpHeaders headers;
+	
+	@Override
+	public KakaoPayReadyResponseVO ready(KakaoPayReadyRequestVO request) throws URISyntaxException {
+		
+		//주소 설정
+		URI uri = new URI("https://kapi.kakao.com/v1/payment/ready");
+		
+		//바디 설정
+		//같은 이름도 처리 가능함 이런 값을 처리하기 위한 Map
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<>(); 
+		body.add("cid", kakaoPayProperties.getCid()); //가맹점번호
+		body.add("partner_order_id", request.getPartnerOrderId()); //시리얼 번호가 랜덤으로 나옴(자바 클래스)
+		body.add("partner_user_id", request.getPartnerUserId()); //구매자 아이디
+		body.add("item_name", request.getItemName()); //구매자 상품명 2개 이상시 외 ?개
+		body.add("quantity", "1"); //무조건 1, 수량은 DB에서 관리할거임
+		body.add("total_amount", String.valueOf(request.getItemPrice())); //결제금액 개발자용은 100만원이 최대 
+		body.add("tax_free_amount", "0"); //면세
+		body.add("approval_url", "http://localhost:8080/pay/success");
+		body.add("cancel_url", "http://localhost:8080/pay/cancel");
+		body.add("fail_url", "http://localhost:8080/pay/fail");
+		
+		//요청 발송
+		HttpEntity entity = new HttpEntity<>(body, headers); //바디+헤더
+		
+		KakaoPayReadyResponseVO response = 
+				template.postForObject(uri, entity, KakaoPayReadyResponseVO.class);
+		
+		return response;
+	}
+}
