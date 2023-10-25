68d32a2cccd8b7e3425354b9ea7d2cf3fe97a4c4
lodorive <s_s0309@naver.com>
2023-10-25 17:12:27 +0900

    클릭으로 구매하는 예제

--- spring21pay/src/main/java/com/kh/spring21/controller/KakaoPayController.java
+++ spring21pay/src/main/java/com/kh/spring21/controller/KakaoPayController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.spring21.dao.ProductDao;
+import com.kh.spring21.dto.ProductDto;
 import com.kh.spring21.service.KakaoPayService;
 import com.kh.spring21.vo.KakaoPayApproveRequestVO;
 import com.kh.spring21.vo.KakaoPayApproveResponseVO;
@@ -97,4 +99,61 @@ public class KakaoPayController {
 		return "redirect:detail?tid="+request.getTid();
 	}
 	
+	//-----------------------------------------------------
+	
+	@Autowired
+	private ProductDao productDao;
+	
+	@RequestMapping("/test2")
+	public String test2(Model model) {
+		model.addAttribute("list", productDao.selectList());
+		return "pay2/home";
+	}
+	
+	@GetMapping("/test2/purchase")
+	public String purchase(HttpSession session, @RequestParam int productNo) throws URISyntaxException {
+		//상품정보 조회
+		ProductDto productDto = productDao.selectOne(productNo);
+		
+		//상품정보를 이용하여 결제준비 요청
+		KakaoPayReadyRequestVO request = KakaoPayReadyRequestVO.builder()
+				.itemName(productDto.getProductName()) //상품명
+				.itemPrice(productDto.getProductPrice()) //상품가격
+				.partnerOrderId(UUID.randomUUID().toString()) //주문번호
+				.partnerUserId("testuser1") //주문자
+			.build();
+		
+		KakaoPayReadyResponseVO response = 
+				kakaoPayService.ready(request);
+						
+		//세션은 아무거나 넣는거 가능
+		session.setAttribute("approve", KakaoPayApproveRequestVO.builder()
+		.partnerOrderId(request.getPartnerOrderId())
+		.partnerUserId(request.getPartnerUserId())
+		.tid(response.getTid())
+		.build()); 
+		
+		//결제페이지를 사용자에게 안내
+		return "redirect:"+response.getNextRedirectPcUrl();
+	}
+	
+	//결제 성공
+	@GetMapping("/test2/purchase/success")
+	public String test2Success(HttpSession session, @RequestParam String pg_token) throws URISyntaxException {
+		//session에 저장되어 있는 flash value를 꺼내에 pg_token을 추가할 뒤 승인 요청
+		KakaoPayApproveRequestVO request = 
+				(KakaoPayApproveRequestVO) session.getAttribute("approve"); //p.o.id, p.u.id, tid 가져옴
+		session.removeAttribute("approve"); //사용 했으니 지워라 
+		
+		request.setPgToken(pg_token); //토큰 추가
+		
+		//결제 승인 요청
+		KakaoPayApproveResponseVO response = kakaoPayService.approve(request);
+		return "redirect:successResult";
+	}
+	
+	@RequestMapping("/test2/purchase/successResult")
+	public String test2SuccessResult() {
+		return "pay2/successResult";
+	}
 }
---
+++ spring21pay/src/main/java/com/kh/spring21/dao/ProductDaoImpl.java
@@ -0,0 +1,29 @@
+package com.kh.spring21.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring21.dto.ProductDto;
+
+@Repository
+public class ProductDaoImpl implements ProductDao{
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	//전체 조회
+	@Override
+	public List<ProductDto> selectList() {
+		return sqlSession.selectList("product.list");
+	}
+	
+	//상세 조회
+	@Override
+	public ProductDto selectOne(int productNo) {
+		return sqlSession.selectOne("product.find", productNo);
+	}
+
+}
