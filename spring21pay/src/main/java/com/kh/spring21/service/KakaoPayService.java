7a05d4c334020c7014a0a9862d149394a3d82aad
lodorive <s_s0309@naver.com>
2023-10-26 11:27:29 +0900

    카카오페이 목록 구매 승인 처리

--- spring21pay/src/main/java/com/kh/spring21/service/KakaoPayService.java
+++ spring21pay/src/main/java/com/kh/spring21/service/KakaoPayService.java
@@ -10,10 +10,14 @@ import com.kh.spring21.vo.KakaoPayDetailRequestVO;
 import com.kh.spring21.vo.KakaoPayDetailResponseVO;
 import com.kh.spring21.vo.KakaoPayReadyRequestVO;
 import com.kh.spring21.vo.KakaoPayReadyResponseVO;
+import com.kh.spring21.vo.PurchaseListVO;
 
 public interface KakaoPayService {
+	//통신
 	KakaoPayReadyResponseVO ready(KakaoPayReadyRequestVO request) throws URISyntaxException;
 	KakaoPayApproveResponseVO approve(KakaoPayApproveRequestVO request) throws URISyntaxException;
 	KakaoPayDetailResponseVO detail(KakaoPayDetailRequestVO request) throws URISyntaxException;
 	KakaoPayCancleResponseVO cancle(KakaoPayCancleRequestVO request) throws URISyntaxException;
+	
+	KakaoPayReadyRequestVO convert(PurchaseListVO listVO);
 }
